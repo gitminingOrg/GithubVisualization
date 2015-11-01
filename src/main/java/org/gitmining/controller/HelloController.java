2f6a3e9bfca7b93f19295e78a8365c8d753ccd22
miracleaaa <miracle@172.17.231.155>
2015-11-01 16:03:53 +0800

    add velocity

--- src/main/java/org/gitmining/controller/HelloController.java
+++ src/main/java/org/gitmining/controller/HelloController.java
@@ -4,6 +4,7 @@ package org.gitmining.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,12 +12,18 @@ import org.springframework.web.servlet.mvc.Controller;
 @RestController
 public class HelloController implements Controller {
 	@RequestMapping(value="/hello")
-	public ModelAndView handleRequest(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
+	public ModelAndView handleRequest(String id, ModelMap modelMap,HttpServletRequest request) throws Exception {
 		// TODO Auto-generated method stub
-		ModelAndView view = new ModelAndView("hello");
-		view.addObject("mes", "hello");
-		return view;
+		//ModelAndView view = new ModelAndView("hello");
+		//view.addObject("mes", "hello");
+		return new ModelAndView("user");
+	}
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
