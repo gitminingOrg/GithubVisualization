5f0c5e727381ca33b78591ac2da0fec175e48daa
miracleaaa <miracle@172.19.111.50>
2015-11-03 14:15:48 +0800

    bug fix

--- src/main/java/org/gitmining/util/DomainRetriever.java
+++ src/main/java/org/gitmining/util/DomainRetriever.java
@@ -3,7 +3,11 @@ package org.gitmining.util;
 public class DomainRetriever {
 	public static String extractBlogDomain(String url){
 		String[] items = url.split("\\.");
-		String contain = items[items.length-2];
+		String contain = items[0];
+		if(items.length>=2){
+			contain=items[items.length-2];	
+		}
+		
 		String[] items2 = contain.split("/");
 		return items2[items2.length-1];
 	}
