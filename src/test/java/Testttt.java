b2186b1540edb71b1bec751aeb26d5720e61d809
zyf <542687316@qq.com>
2016-04-11 14:08:44 +0800

    .

--- src/test/java/Testttt.java
+++ src/test/java/Testttt.java
@@ -10,4 +10,23 @@ public class Testttt {
 		assertEquals(null, "123", "123");
 	}
 
+	@Test
+	public void test1() {
+		assertEquals(null, "123", "12");
+	}
+	
+	@Test
+	public void test2() {
+		assertEquals(null, "123", "123");
+	}
+	
+	@Test
+	public void test3() {
+		assertEquals(null, "123", "12");
+	}
+	
+	@Test
+	public void test4() {
+		assertEquals(null, "123", "123");
+	}
 }
